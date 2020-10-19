18031a8221c01577ef9d669df6aa8b54275b354e
claugf <63432597+claugf@users.noreply.github.com>
2020-10-19 02:41:35 +0100

    first

---
+++ controllers/authors.js
@@ -0,0 +1,25 @@
+const authors = require('../models/authors.js')();
+
+module.exports = () => {
+  const getController = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.json(authors.get());
+  }
+
+  const getById = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.json(authors.get(req.params.id))
+  }
+
+  const postController = (req, res) => {
+    const name = req.body.name;
+    authors.add(name);
+    return res.end(`POST: ${name}`);
+  }
+
+  return {
+    getController,
+    postController,
+    getById
+  }
+}
